7a7b092f4d7b6de21b5cd283b854428b2c15c882
Siegfred Balona <siegblink@gmail.com>
2019-07-08 20:13:02 -0400

    Add send email to multiple receipients functionality

--- controller/emailSender.js
+++ controller/emailSender.js
@@ -6,21 +6,11 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 module.exports = function sendEmail(formData) {
   const { agentEmail, clientEmail, flightInfo } = formData
   const msg = {
-    to: clientEmail,
+    to: [clientEmail, agentEmail],
     from: agentEmail,
     subject: 'Order Details',
-    text: 'Order Details',
     html: flightInfo,
   }
 
-  sgMail.send(msg)
+  sgMail.sendMultiple(msg)
 }
-
-// agentEmail: 'siegblink@gmail.com'
-// clientEmail: 'siegblink@gmail.com'
-// clientName: 'Siegfred Balona'
-// flightInfo: '<p>Hello customer,</p><p><br></p><p>The following are your flight order details.</p>'
-// numPassengers: '2'
-// price: '100'
-// tax: '50'
-// total: '150'
