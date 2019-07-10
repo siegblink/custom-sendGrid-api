61c0bbadf23ece9d40bbce62b4ef717f41fee70b
Siegfred Balona <siegblink@gmail.com>
2019-07-10 16:06:24 +0800

    Resolve deploy conflicts

---
+++ config/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rebrandlyApiKey: '440ee4c0e225447889c40bdf984dc45d',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDhU553Z6pMtiC9PfrlPjo0SKbdK24LG08',
+    authDomain: 'code-challenge-4b56f.firebaseapp.com',
+    databaseURL: 'https://code-challenge-4b56f.firebaseio.com',
+    projectId: 'code-challenge-4b56f',
+    storageBucket: 'code-challenge-4b56f.appspot.com',
+    messagingSenderId: '944350373611',
+    appId: '1:944350373611:web:973b05f579a0bca0',
+  },
+}
